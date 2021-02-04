b740fbf54c600d2ebd9e2f7e9ec2dd1636877e87
Kiki Dawson <kikiadawson@gmail.com>
2021-02-04 12:21:45 +0000

    react form showing on post page

--- app/assets/javascripts/PostsPage.js
+++ app/assets/javascripts/PostsPage.js
@@ -4,57 +4,67 @@
 
 const p = React.createElement;
 
+class PostForm extends React.Component {
+  constructor(props) {
+    // console.log(props),
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    alert('A message was submitted: ' + this.state.value);
+  }
+
+  render() {
+    return p(
+      'form',
+      { onSubmit: this.handleSubmit },
+      [
+        p("textarea", { value: this.state.value, onChange: this.handleChange }, undefined),
+        p('button', undefined, "Submit")
+      ]
+    )
+  }
+}
+
+
 class PostPage extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    list: []
-  }
-  this.getData()
-  }
-// call to API
-getData() {
-  const url = '/index_API'
-  let postsDiv = document.getElementById('display-posts')
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+    this.getData()
+    }
+  // call to API
+  getData() {
+    const url = '/index_API'
+    let postsDiv = document.getElementById('display-posts')
 
-  fetch(url)
-  .then(response => response.json())
-  .then(result => {
-    console.log(result)
-    this.setState({list: result})
-    // result.forEach(post => {
-      // this needs some attention
-      // let stylingOneDiv = document.createElement('div')
-      // stylingOneDiv.className = "row mb-4"
-      // let stylingTwoDiv = document.createElement('div')
-      // stylingTwoDiv.className = "col-xs-4 col-xs-offset-4"
-      // let postDiv = document.createElement('div')
-      // postDiv.className = "p-3 border border-dark"
-      // let html = `
-      //   <p>${post.message}</p>
-      //   <p>created at ${post.created_at}</p>
-      //   <p>created by ${post.username}</p>
-      // `
-      // postDiv.innerHTML = html
-      // stylingOneDiv.appendChild(stylingTwoDiv)
-      // stylingTwoDiv.appendChild(postDiv)
-      // postsDiv.appendChild(stylingOneDiv)
-    // })
-  }
-  )}
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      this.setState({list: result})
+    }
+    )}
 
-//     // grabs all data in the post array
-//     console.log(result)
-//     //grab data from 1st index in the array with the message
-//     console.log(result[0].message)
-//   })
-// }
 
-render() {
-  return p(
+
+  render() {
+    return p(
       "div",
       undefined,
       this.state.list.map((post_data) => {
+        console.log(post_data.id)
         return p(
           "div",
           { className: "row mb-4" },
@@ -66,7 +76,7 @@ render() {
         );
       })
     );
- }
+  }
 }
 class Post extends React.Component {
   // constructor(props) {
@@ -80,40 +90,45 @@ class Post extends React.Component {
       //   [...children]
       // )
       'div', //type of element.it can also be react component ----> call this script before PostPage
-      {className: "p-3 border border-dark"}, //html tag --> attribute of the tag; react component --> props
+      { className: "p-3 border border-dark"}, //html tag --> attribute of the tag; react component --> props
       [
-      p("p", undefined, this.props.message),
-      p("p", undefined, `created at ${this.props.created_at}`),
-      p("p", undefined, `created by ${this.props.username}`)
+        p("p", undefined, this.props.message),
+        p("p", undefined, `created at ${this.props.created_at}`),
+        p("p", undefined, `created by ${this.props.username}`)
       ///like button --> create another class
-    ] // content of the post --> 3 p element children
+      ] // content of the post --> 3 p element children
     );
   }
-  }
+}
+
 const pageContainer = document.getElementById('posts-page');
 ReactDOM.render(p(PostPage), pageContainer);
 
+const formContainer = document.getElementById('post-form');
+ReactDOM.render(p(PostForm), formContainer);
 
-// 'use strict';
-// console.log("im in posts")
-// var postsDiv = document.getElementById('posts-page')
-// const url ='/index_API'
 
-// fetch(url)
-// .then(response => response.json())
-// .then(data => {
 
-//     console.log('data')
-//     console.log(data)
-//     data.forEach(post => {
-//         let postDiv= document.createElement('div')
-//         let html= `
-//         <p>${post.message}</p>
-//         <p>added at ${post.created_at}</p>
-//         <p>added by ${post.username}</p>
-//         `
-//         postDiv.innerHTML = html
-//         postsDiv.appendChild(postDiv)
-//     })
+
+// "-------------"
+// class Form extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { username: '' }
+//   }
+//
+//   handleChange(event) {}
+//
+//   render() {
+//     return (
+//       <form>
+//         Username:
+//         <input
+//           type="text"
+//           value={this.state.username}
+//           onChange={this.handleChange}
+//         />
+//       </form>
+//     )
+//   }
 // }
-// )
