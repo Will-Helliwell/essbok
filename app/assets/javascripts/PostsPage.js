e78d446fa986d406899b9a6d802643fe4e7dc3ea
ZeenLamDev <Zeen.Lam93@gmail.com>
2021-02-04 18:33:21 +0000

    create post now working and updating state to new array of posts

--- app/assets/javascripts/PostsPage.js
+++ app/assets/javascripts/PostsPage.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // console.log("hi")
 
@@ -31,22 +31,12 @@ class PostForm extends React.Component {
       },
       method: "POST",
       body: JSON.stringify({
-        message: this.state.message
+        message: this.state.message,
       }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-      })
-      // .catch((err) => {
-      //   // console.log(err);
-      // });
+    }).then(() => {
+      this.props.onCreateSuccess();
+    });
   }
-
-  // .then((response) => {return response.json()})
-  //         .then((fruit) => {this.setState({ filtered: [...this.state.filtered, fruit] })
-  //          });
-  //   }
 
   render() {
     return p(
@@ -57,97 +47,98 @@ class PostForm extends React.Component {
       p(
         "label",
         null,
-        p(
-          "textarea",
-          {
-            message: this.state.value,
-            onChange: this.handleChange,
-          }
-        )
+        p("textarea", {
+          message: this.state.value,
+          onChange: this.handleChange,
+        })
       ),
-      p(
-        "input",
-        {
-          type: "submit",
-          value: "Submit",
-        },
-      )
+      p("input", {
+        type: "submit",
+        value: "Submit",
+      })
     );
   }
 }
-
-
 
 class PostPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: []
-    }
-    this.getData()
-    }
+      list: [],
+    };
+    this.getData();
+  }
   // call to API
   getData() {
-    const url = '/index_API'
-    let postsDiv = document.getElementById('display-posts')
+    const url = "/index_API";
+    let postsDiv = document.getElementById("display-posts");
 
     fetch(url)
-    .then(response => response.json())
-    .then(result => {
-      this.setState({list: result})
-    })
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({ list: result });
+      });
   }
 
   render() {
-    return p(
-      "div",
-      undefined,
-      this.state.list.map((post_data) => {
-        // console.log(post_data.id)
-        return p(
-          "div",
-          { className: "row mb-4" },
-          p(
+    return [
+      p(
+        PostForm,
+        {
+          onCreateSuccess: () => {
+            console.log("TRIGGER");
+            this.getData();
+          },
+        },
+        undefined
+      ),
+      p(
+        "div",
+        undefined,
+        this.state.list.map((post_data) => {
+          // console.log(post_data.id)
+          return p(
             "div",
-            { className: "col-xs-4 col-xs-offset-4" },
-            p(Post, post_data, undefined)
-          )
-        );
-      })
-    );
+            { className: "row mb-4" },
+            p(
+              "div",
+              { className: "col-xs-4 col-xs-offset-4" },
+              p(Post, post_data, undefined)
+            )
+          );
+        })
+      ),
+    ];
   }
 }
 class Post extends React.Component {
   // constructor(props) {
   //   super(props);
-    // }
-    render() {
+  // }
+  render() {
     return p(
       // React.createElement(
       //   type,
       //   [props],
       //   [...children]
       // )
-      'div', //type of element.it can also be react component ----> call this script before PostPage
-      { className: "p-3 border border-dark"}, //html tag --> attribute of the tag; react component --> props
+      "div", //type of element.it can also be react component ----> call this script before PostPage
+      { className: "p-3 border border-dark" }, //html tag --> attribute of the tag; react component --> props
       [
         p("p", undefined, this.props.message),
         p("p", undefined, `created at ${this.props.created_at}`),
-        p("p", undefined, `created by ${this.props.username}`)
-      ///like button --> create another class
+        p("p", undefined, `created by ${this.props.username}`),
+        ///like button --> create another class
       ] // content of the post --> 3 p element children
     );
   }
 }
 
-const pageContainer = document.getElementById('posts-page');
+const pageContainer = document.getElementById("posts-page");
 ReactDOM.render(p(PostPage), pageContainer);
 
-const formContainer = document.getElementById('post-form');
+const formContainer = document.getElementById("post-form");
 ReactDOM.render(p(PostForm), formContainer);
-
-
-
 
 // "-------------"
 // class Form extends React.Component {
